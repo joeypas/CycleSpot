7d5506e57580335df97669627845a597f8bc36c3
Joe Liotta <jliotta03@gmail.com>
2025-10-15 15:36:05 -0400

    Initial Commit

---
+++ CycleSpot/CycleSpotApp.swift
@@ -0,0 +1,17 @@
+//
+//  CycleSpotApp.swift
+//  CycleSpot
+//
+//  Created by Joseph Liotta on 10/15/25.
+//
+
+import SwiftUI
+
+@main
+struct CycleSpotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CycleSpotTests/CycleSpotTests.swift
@@ -0,0 +1,17 @@
+//
+//  CycleSpotTests.swift
+//  CycleSpotTests
+//
+//  Created by Joseph Liotta on 10/15/25.
+//
+
+import Testing
+@testable import CycleSpot
+
+struct CycleSpotTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
