801dad49c56e6e89aca00883f9f6dade75441ed5
Remy Roel <112566889+remyroel@users.noreply.github.com>
2025-10-18 20:33:02 -0400

    Introduce Friend struct and friends list in FriendsView
    Added a Friend struct and preloaded friends list.

--- CycleSpot/FriendsView.swift
+++ CycleSpot/FriendsView.swift
@@ -6,9 +6,30 @@
 //
 
 import SwiftUI
+struct Friend: Identifiable, Hashable {
+    let id = UUID()
+    let name: String
+}
 
 struct FriendsView: View {
+    // Preloaded friends
+    private let friends: [Friend] = [
+        Friend(name: "Owen Romeo"),
+        Friend(name: "Joe Liotta"),
+        Friend(name: "Remy Roel"),
+        Friend(name: "Kiera Hanson")
+    ]
+    
     var body: some View {
-        
+        NavigationStack {
+            List(friends) { friend in
+                Label(friend.name, systemImage: "person.circle")
+            }
+            .navigationTitle("Friends")
+        }
     }
+}
+
+#Preview {
+    ContentView()
 }
